f188af61acb8e03993b851329ed89e65b8950362
Jong Chern <jongchern@gmail.com>
2021-10-21 20:29:18 +0800

    updates, now supports bin fil sync

--- ShimmerBasicExamples/src/main/java/com/shimmerresearch/simpleexamples/SensorMapsExample.java
+++ ShimmerBasicExamples/src/main/java/com/shimmerresearch/simpleexamples/SensorMapsExample.java
@@ -43,13 +43,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.awt.Canvas;
 
-
-
 public class SensorMapsExample extends BasicProcessWithCallBack {
-	
+
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField btFriendlyNameTextField;
+	private JTextField ParticipantNameTextField;
+	private JTextField TrialNameTextField;
+	private JLabel lblParticipantName;
+	private JLabel lblTrialName;
+	private JButton btnSync;
+	private JLabel lblPayloadIndex;
 	JTextPane textPaneStatus;
 	static ShimmerDevice shimmerDevice;
 	static BasicShimmerBluetoothManagerPc btManager = new BasicShimmerBluetoothManagerPc();
@@ -57,9 +61,10 @@ public class SensorMapsExample extends BasicProcessWithCallBack {
 	String btComport;
 	String macAddress;
 	String btFriendlyName;
-	
+
 	/**
 	 * Initialize the contents of the frame
+	 * 
 	 * @wbp.parser.entryPoint
 	 */
 	public void initialize() {
@@ -67,43 +72,42 @@ public class SensorMapsExample extends BasicProcessWithCallBack {
 		frame.setBounds(100, 100, 662, 591);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblSetComPort = new JLabel("Set COM Port or Mac Id");
-		lblSetComPort.setBounds(10, 100, 154, 23);
+		lblSetComPort.setBounds(10, 85, 154, 23);
 		frame.getContentPane().add(lblSetComPort);
-		
+
 		textField = new JTextField();
 		textField.setToolTipText("for example COM1, COM2, d0:2b:46:3d:a2:bb, etc");
-		textField.setBounds(10, 121, 154, 29);
+		textField.setBounds(10, 106, 154, 29);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblSetFriendlyName = new JLabel("Set Friendly Name");
-		lblSetFriendlyName.setBounds(10, 40, 154, 23);
+		lblSetFriendlyName.setBounds(10, 30, 154, 23);
 		frame.getContentPane().add(lblSetFriendlyName);
-		
+
 		btFriendlyNameTextField = new JTextField();
 		btFriendlyNameTextField.setToolTipText("for example Verisense-19092501A2BB, Shimmer3-1E59, etc");
-		btFriendlyNameTextField.setBounds(10, 61, 154, 29);
+		btFriendlyNameTextField.setBounds(10, 51, 154, 29);
 		frame.getContentPane().add(btFriendlyNameTextField);
 		btFriendlyNameTextField.setColumns(10);
-		
-		//textField.setText("e7:45:2c:6d:6f:14");
-		//textField.setText("d0:2b:46:3d:a2:bb");
-		textField.setText("e7:ec:37:a0:d2:34");
-		//textField.setText("Com5");
-		//textField2.setText("Shimmer-E6C8");
+
+		// textField.setText("e7:45:2c:6d:6f:14");
+		textField.setText("d0:2b:46:3d:a2:bb");
+		// textField.setText("e7:ec:37:a0:d2:34");
+		// textField.setText("Com5");
+		// textField2.setText("Shimmer-E6C8");
 		btFriendlyNameTextField.setText("Verisense-19092501A2BB");
-		
+
 		JButton btnConnect = new JButton("CONNECT");
 		btnConnect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				btFriendlyName = btFriendlyNameTextField.getText();
-				if(btFriendlyName.contains("Shimmer")) {
+				if (btFriendlyName.contains("Shimmer")) {
 					btComport = textField.getText();
 					btManager.connectShimmerThroughCommPort(btComport);
-				}
-				else if(btFriendlyName.contains("Verisense")) {
+				} else if (btFriendlyName.contains("Verisense")) {
 					macAddress = textField.getText();
 					btManager.setPathToVeriBLEApp("bleconsoleapp\\BLEConsoleApp1.exe");
 					BluetoothDeviceDetails devDetails = new BluetoothDeviceDetails("", macAddress, "Verisense");
@@ -112,9 +116,53 @@ public class SensorMapsExample extends BasicProcessWithCallBack {
 			}
 		});
 		btnConnect.setToolTipText("attempt connection to Shimmer device");
-		btnConnect.setBounds(210, 90, 175, 31);
+		btnConnect.setBounds(210, 35, 175, 31);
 		frame.getContentPane().add(btnConnect);
-		
+
+		lblParticipantName = new JLabel("Participant Name");
+		lblParticipantName.setBounds(415, 30, 175, 23);
+		frame.getContentPane().add(lblParticipantName);
+
+		ParticipantNameTextField = new JTextField();
+		ParticipantNameTextField.setToolTipText("enter your participant name");
+		ParticipantNameTextField.setBounds(415, 51, 175, 29);
+		frame.getContentPane().add(ParticipantNameTextField);
+		ParticipantNameTextField.setColumns(10);
+
+		lblTrialName = new JLabel("Trial Name");
+		lblTrialName.setBounds(415, 85, 175, 23);
+		frame.getContentPane().add(lblTrialName);
+
+		TrialNameTextField = new JTextField();
+		TrialNameTextField.setToolTipText("enter the trial name");
+		TrialNameTextField.setBounds(415, 106, 175, 29);
+		frame.getContentPane().add(TrialNameTextField);
+		TrialNameTextField.setColumns(10);
+
+		lblPayloadIndex = new JLabel("Current payload index :");
+		lblPayloadIndex.setBounds(415, 150, 175, 23);
+		frame.getContentPane().add(lblPayloadIndex);
+
+		btnSync = new JButton("DATA SYNC");
+		btnSync.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				VerisenseDevice verisenseDevice = (VerisenseDevice) shimmerDevice;
+				verisenseDevice.setTrialName(TrialNameTextField.getText());
+				verisenseDevice.setParticipantID(ParticipantNameTextField.getText());
+				verisenseDevice.readLoggedData();
+			}
+		});
+		btnSync.setToolTipText("Data Sync");
+		btnSync.setBounds(210, 199, 175, 31);
+		frame.getContentPane().add(btnSync);
+
+		ParticipantNameTextField.setVisible(false);
+		lblParticipantName.setVisible(false);
+		TrialNameTextField.setVisible(false);
+		lblTrialName.setVisible(false);
+		btnSync.setVisible(false);
+		lblPayloadIndex.setVisible(false);
+
 		JButton btnDisconnect = new JButton("DISCONNECT");
 		btnDisconnect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -122,59 +170,63 @@ public class SensorMapsExample extends BasicProcessWithCallBack {
 			}
 		});
 		btnDisconnect.setToolTipText("disconnect from Shimmer device");
-		btnDisconnect.setBounds(415, 90, 175, 31);
+		btnDisconnect.setBounds(210, 76, 175, 31);
 		frame.getContentPane().add(btnDisconnect);
-		
+
 		JLabel lblShimmerStatus = new JLabel("Shimmer Status");
-		lblShimmerStatus.setBounds(10, 160, 154, 23);
+		lblShimmerStatus.setBounds(10, 145, 154, 23);
 		frame.getContentPane().add(lblShimmerStatus);
-		
+
 		textPaneStatus = new JTextPane();
-		textPaneStatus.setBounds(10, 181, 154, 29);
+		textPaneStatus.setBounds(10, 166, 154, 35);
 		frame.getContentPane().add(textPaneStatus);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 638, 23);
 		frame.getContentPane().add(menuBar);
-		
+
 		JMenu mnTools = new JMenu("Tools");
 		menuBar.add(mnTools);
-		
+
 		JMenuItem mntmSelectSensors = new JMenuItem("Select sensors");
 		mntmSelectSensors.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				//Ensure the Shimmer is not streaming or SD logging before configuring it
-				if(shimmerDevice.isConnected()) {
-					if(!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
+
+				// Ensure the Shimmer is not streaming or SD logging before configuring it
+				if (shimmerDevice.isConnected()) {
+					if (!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
 						EnableSensorsDialog sensorsDialog;
 						sensorsDialog = new EnableSensorsDialog(shimmerDevice, btManager);
 						sensorsDialog.showDialog();
 					} else {
-						JOptionPane.showMessageDialog(frame, "Cannot configure sensors!\nDevice is streaming or SDLogging", "Warning", JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(frame,
+								"Cannot configure sensors!\nDevice is streaming or SDLogging", "Warning",
+								JOptionPane.WARNING_MESSAGE);
 					}
 				} else {
 					JOptionPane.showMessageDialog(frame, "No device connected!", "Info", JOptionPane.WARNING_MESSAGE);
 				}
-				
+
 //					EnableSensorsDialog sensorsDialog = new EnableSensorsDialog(shimmerDevice);
 //					sensorsDialog.initialize();
-				
+
 			}
 		});
 		mnTools.add(mntmSelectSensors);
-		
+
 		JMenuItem mntmDeviceConfiguration = new JMenuItem("Sensor configuration");
 		mntmDeviceConfiguration.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				if(shimmerDevice.isConnected()) {
-					if(!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
+
+				if (shimmerDevice.isConnected()) {
+					if (!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
 						SensorConfigDialog configDialog;
-						configDialog = new SensorConfigDialog(shimmerDevice,btManager);
+						configDialog = new SensorConfigDialog(shimmerDevice, btManager);
 						configDialog.showDialog();
 					} else {
-						JOptionPane.showMessageDialog(frame, "Cannot configure sensors!\nDevice is streaming or SDLogging", "Warning", JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(frame,
+								"Cannot configure sensors!\nDevice is streaming or SDLogging", "Warning",
+								JOptionPane.WARNING_MESSAGE);
 					}
 				} else {
 					JOptionPane.showMessageDialog(frame, "No device connected!", "Info", JOptionPane.WARNING_MESSAGE);
@@ -182,52 +234,51 @@ public class SensorMapsExample extends BasicProcessWithCallBack {
 			}
 		});
 		mnTools.add(mntmDeviceConfiguration);
-		
-		
+
 		JPanel plotPanel = new JPanel();
 		plotPanel.setBounds(10, 236, 611, 272);
 		frame.getContentPane().add(plotPanel);
 		plotPanel.setLayout(null);
-		
+
 		final Chart2D mChart = new Chart2D();
 		mChart.setLocation(12, 13);
 		mChart.setSize(587, 246);
 		plotPanel.add(mChart);
 		plotManager.addChart(mChart);
-		
+
 		JMenuItem mntmSignalsToPlot = new JMenuItem("Signals to plot");
 		mntmSignalsToPlot.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				SignalsToPlotDialog signalsToPlotDialog;
-				if(btFriendlyName.contains("Verisense")) {
+				if (btFriendlyName.contains("Verisense")) {
 					signalsToPlotDialog = new SignalsToPlotDialog(true);
-				}
-				else {
+				} else {
 					signalsToPlotDialog = new SignalsToPlotDialog();
 				}
-				
+
 				signalsToPlotDialog.initialize(shimmerDevice, plotManager, mChart);
 			}
 		});
-		
+
 		mnTools.add(mntmSignalsToPlot);
-		
+
 		JButton btnStartStreaming = new JButton("START STREAMING");
 		btnStartStreaming.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				try {
 					shimmerDevice.startStreaming();
 				} catch (ShimmerException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+					textPaneStatus.setText(e.getMessage());
 				}
-				
+
 			}
 		});
-		btnStartStreaming.setBounds(210, 181, 175, 31);
+		btnStartStreaming.setBounds(210, 117, 175, 31);
 		frame.getContentPane().add(btnStartStreaming);
-		
+
 		JButton btnStopStreaming = new JButton("STOP STREAMING");
 		btnStopStreaming.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -239,81 +290,106 @@ public class SensorMapsExample extends BasicProcessWithCallBack {
 				}
 			}
 		});
-		btnStopStreaming.setBounds(415, 181, 175, 31);
+		btnStopStreaming.setBounds(210, 158, 175, 31);
 		frame.getContentPane().add(btnStopStreaming);
-		
-		plotManager.setTitle("Plot");		
+
+		plotManager.setTitle("Plot");
 	}
 
 	public static void main(String args[]) {
-		//shimmer
+		// shimmer
 		SensorMapsExample s = new SensorMapsExample();
-		
+
 		s.initialize();
 		s.frame.setVisible(true);
-		s.setWaitForData(btManager.callBackObject);		
+		s.setWaitForData(btManager.callBackObject);
 		s.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		s.frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				if (shimmerDevice instanceof VerisenseDevice) {
-					((VerisenseDevice)shimmerDevice).stopCommunicationProcess(COMMUNICATION_TYPE.BLUETOOTH);
+					((VerisenseDevice) shimmerDevice).stopCommunicationProcess(COMMUNICATION_TYPE.BLUETOOTH);
 				}
 			}
 		});
-		//s.setWaitForData(shimmer);
+		// s.setWaitForData(shimmer);
 	}
-	
+
 	@Override
 	protected void processMsgFromCallback(ShimmerMsg shimmerMSG) {
 		// TODO Auto-generated method stub
 
 		// TODO Auto-generated method stub
-		  int ind = shimmerMSG.mIdentifier;
+		int ind = shimmerMSG.mIdentifier;
 
-		  Object object = (Object) shimmerMSG.mB;
+		Object object = (Object) shimmerMSG.mB;
 
 		if (ind == ShimmerPC.MSG_IDENTIFIER_STATE_CHANGE) {
-			CallbackObject callbackObject = (CallbackObject)object;
-			
+			CallbackObject callbackObject = (CallbackObject) object;
+
 			if (callbackObject.mState == BT_STATE.CONNECTING) {
 				textPaneStatus.setText("connecting...");
 			} else if (callbackObject.mState == BT_STATE.CONNECTED) {
 				textPaneStatus.setText("connected");
-				
-				String btComportOrMacAddress = btFriendlyName.contains("Verisense") ? macAddress.toUpperCase() : btComport;
-				shimmerDevice = btManager.getShimmerDeviceBtConnected(btComportOrMacAddress);
-				
-				//shimmer.startStreaming();
+
+				if (btFriendlyName.contains("Verisense")) {
+					ParticipantNameTextField.setVisible(true);
+					lblParticipantName.setVisible(true);
+					TrialNameTextField.setVisible(true);
+					lblTrialName.setVisible(true);
+					btnSync.setVisible(true);
+					lblPayloadIndex.setVisible(false);
+
+					shimmerDevice = btManager.getShimmerDeviceBtConnected(macAddress.toUpperCase());
+				} else {
+					shimmerDevice = btManager.getShimmerDeviceBtConnected(btComport);
+				}
+
+				// shimmer.startStreaming();
 			} else if (callbackObject.mState == BT_STATE.DISCONNECTED
 //					|| callbackObject.mState == BT_STATE.NONE
-					|| callbackObject.mState == BT_STATE.CONNECTION_LOST){
-				textPaneStatus.setText("disconnected");				
+					|| callbackObject.mState == BT_STATE.CONNECTION_LOST) {
+				textPaneStatus.setText("disconnected");
+
+				ParticipantNameTextField.setVisible(false);
+				lblParticipantName.setVisible(false);
+				TrialNameTextField.setVisible(false);
+				lblTrialName.setVisible(false);
+				btnSync.setVisible(false);
+				lblPayloadIndex.setVisible(false);
+			} else if (callbackObject.mState == BT_STATE.STREAMING) {
+				textPaneStatus.setText("device streaming");
+			}else if (callbackObject.mState == BT_STATE.STREAMING_LOGGED_DATA) {
+				textPaneStatus.setText("synchronizing data");
+				lblPayloadIndex.setVisible(true);
+
+				VerisenseDevice verisenseDevice = (VerisenseDevice) shimmerDevice;
+				lblPayloadIndex.setText("Current Payload Index : " + verisenseDevice.getPayloadIndex());
 			}
 		} else if (ind == ShimmerPC.MSG_IDENTIFIER_NOTIFICATION_MESSAGE) {
-			CallbackObject callbackObject = (CallbackObject)object;
+			CallbackObject callbackObject = (CallbackObject) object;
 			int msg = callbackObject.mIndicator;
-			if (msg== ShimmerPC.NOTIFICATION_SHIMMER_FULLY_INITIALIZED){
+			if (msg == ShimmerPC.NOTIFICATION_SHIMMER_FULLY_INITIALIZED) {
 				textPaneStatus.setText("device fully initialized");
 			}
 			if (msg == ShimmerPC.NOTIFICATION_SHIMMER_STOP_STREAMING) {
 				textPaneStatus.setText("device stopped streaming");
 			} else if (msg == ShimmerPC.NOTIFICATION_SHIMMER_START_STREAMING) {
 				textPaneStatus.setText("device streaming");
-			} else {}
+			} else {
+			}
 		} else if (ind == ShimmerPC.MSG_IDENTIFIER_DATA_PACKET) {
 			System.out.println("Shimmer MSG_IDENTIFIER_DATA_PACKET");
 			ObjectCluster objc = (ObjectCluster) shimmerMSG.mB;
-			
+
 			try {
 				plotManager.filterDataAndPlot(objc);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 		} else if (ind == ShimmerPC.MSG_IDENTIFIER_PACKET_RECEPTION_RATE_OVERALL) {
-			
+
 		}
 	}
 }
-
